62dae2ac2db8a3acfb2f5966d9b2adf99d48056b
TarasRom <tarasromanushun@gmail.com>
2024-09-07 09:33:53 +0300

    add

---
+++ src/lab2/EvenChecker.java
@@ -0,0 +1,23 @@
+package lab2;
+
+import java.util.Scanner;
+
+public class EvenChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number: ");
+        if (scanner.hasNextInt()) {
+            int number = scanner.nextInt();
+            System.out.println("Number is even: " + isEven(number));
+        } else {
+            System.out.println("Invalid number! Please, enter integer number");
+        }
+
+        scanner.close();
+    }
+
+    public static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+}
