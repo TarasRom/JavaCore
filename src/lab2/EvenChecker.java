23749218ea6553494aa55cfd9db8659bdef63e52
TarasRom <tarasromanushun@gmail.com>
2024-09-15 11:05:02 +0300

    added lab3

--- src/lab2/EvenChecker.java
+++ src/lab2/EvenChecker.java
@@ -5,21 +5,18 @@ import java.util.Scanner;
 public class EvenChecker {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Enter the number: ");
 
         if (scanner.hasNextInt()) {
             int number = scanner.nextInt();
             System.out.println("The number is even: " + isEven(number));
         } else {
-            System.out.println("Invalid number! Please, enter integer number.");
+            System.out.println("Invalid number. Enter the decimal number");
         }
-
         scanner.close();
     }
 
-    public static boolean isEven(int number) {
+    private static boolean isEven(int number) {
         return number % 2 == 0;
     }
 }
-
